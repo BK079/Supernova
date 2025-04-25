34e3343862de6d0c4e7396346186c7012d9afa0b
Lankyrobot <campbellwright@xtra.co.nz>
2025-04-25 21:55:44 +1200

    Universe and adjustments to player

--- supernova/PlayerStar.gd
+++ supernova/PlayerStar.gd
@@ -5,14 +5,17 @@ var thrust = 200
 @onready var heat = 200
 
 
-
 func _integrate_forces(state):
 	var disttomouse = get_global_mouse_position() - get_global_position()
 	if Input.is_action_pressed("Onmouseleft"):
 		state.apply_force(disttomouse)
 		mass -= 1
+		print("test")
 	if Input.is_action_pressed("Onmouseright"):
 		state.apply_force(disttomouse)
 		heat -= 1
-	print(mass)
-	print(heat)
+		print("test")
+	if Input.is_action_just_pressed("Scrollin"):
+		get_node("Camera2D").set_zoom(-Vector2(0.1, 0.1))
+	if Input.is_action_just_pressed("Scrollout"):
+		get_node("Camera2D").set_zoom(+Vector2(0.1, 0.1))
