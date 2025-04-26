f640e0975b92461974e0b850d1bf01dfc61567f2
Lankyrobot <campbellwright@xtra.co.nz>
2025-04-27 11:15:20 +1200

    Particle integration with main changes (hopefully)

---
+++ supernova/jet.gd
@@ -0,0 +1,27 @@
+extends GPUParticles2D
+
+func _physics_process(delta: float) -> void:
+	var heat = clamp(get_parent().heat, 0, 1000)
+	
+	
+	scale = Vector2(heat, heat) * 0.01
+	
+	
+
+	if heat >= 50:
+		self.modulate = Color(1, 0, 0)
+	if heat >= 150:
+		self.modulate = Color(0.8, 0.2, 0)
+	if heat >= 300:
+		self.modulate = Color(0.5, 0.5, 0)
+	if heat >= 450:
+		self.modulate = Color(0, 1, 0)
+	if heat >= 550:
+		self.modulate = Color(0.2, 0.6, 0.2)
+	if heat >= 700:
+		self.modulate = Color(1, 1, 1)
+	if heat >= 825:
+		self.modulate = Color(1, 0, 1)
+	if heat >= 900:
+		self.modulate = Color(0, 0.5, 1)
+	#self.modulate = Color(lerp())
---
+++ supernova/mass.gd
@@ -0,0 +1,31 @@
+extends GPUParticles2D
+
+
+func _physics_process(delta: float) -> void:
+	var mass = clamp(get_parent().mass, 0, 1000)
+	var heat = clamp(get_parent().heat, 0, 1000)
+	
+	var density = get_parent().scale
+	
+
+	scale = Vector2(mass, heat) * 0.002
+	
+	self.process_material.set("scale", scale*density)
+	if mass >= 50:
+		self.modulate = Color(1, 0, 0)
+	if mass >= 150:
+		self.modulate = Color(0.8, 0.2, 0)
+	if mass >= 300:
+		self.modulate = Color(0.5, 0.5, 0)
+	if mass >= 450:
+		self.modulate = Color(0, 1, 0)
+	if mass >= 550:
+		self.modulate = Color(0.2, 0.6, 0.2)
+	if mass >= 700:
+		self.modulate = Color(1, 1, 1)
+	if mass >= 825:
+		self.modulate = Color(1, 0, 1)
+	if mass >= 900:
+		self.modulate = Color(0, 0.5, 1)
+	
+	
